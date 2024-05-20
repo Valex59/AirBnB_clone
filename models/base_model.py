e8b202530cffa4219379f503a93d2880f733f9d5
Valex59 <valex4real5929@gmail.com>
2024-05-20 23:02:15 +0000

    Task 3 Solved

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,9 @@ class BaseModel:
                 instance of the BaseModel
         updated_at: a datetime object representing the last update date of
                 the instance of the BaseModel
-        """
+    """
 
-    """def __init__(self, *args, **kwargs):"""
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new BaseModel instance
 
         Args:
@@ -48,11 +46,6 @@ class BaseModel:
 
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        """
-
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -65,7 +58,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            """
 
     def __str__(self):
         """A string representation of the BaseModel is returned."""
