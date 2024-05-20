e6e1c5792f6b03e1163b89bba48d1adaf28fc18e
Valex59 <valex4real5929@gmail.com>
2024-05-20 16:26:19 +0000

    A beautiful code that passes pycodestyle check that adds two numbers

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
+
 """
 This module provides basic mathematical operations.
+
+This module includes a function to add two integers and return their sum.
 """
 
 
 def add_numbers(number1: int, number2: int) -> int:
     """
-    Adds two integers and returns the result.
+    Adds two integers and returns the sum.
 
     Args:
-    number1 (int): The first number.
-    number2 (int): The second number.
+        number1 (int): The first integer.
+        number2 (int): The second integer.
 
     Returns:
         int: The sum of number1 and number2.
-    """
-    return number1 + number2
 
+    Example:>>> add_numbers(5, 3)
+        8
+    """
+
+
+    return number1 + number2
 
 if __name__ == "__main__":
     num1 = 10
